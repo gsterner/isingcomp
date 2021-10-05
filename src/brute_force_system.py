6a4135cfc89df801411d83f50aee40e668758cda
Gustaf Lundborg <gsterner@gmail.com>
2021-10-05 22:47:23 +0200

    Refactors spin generation

--- src/brute_force_system.py
+++ src/brute_force_system.py
@@ -16,10 +16,22 @@ def generate_children_at_position(config, position):
     pos_config[position] = 1
     return [neg_config, pos_config]
 
+def add_element(config):
+    neg_config = copy.deepcopy(config)
+    neg_config.append(-1)
+    pos_config = copy.deepcopy(config)
+    pos_config.append(1)
+    return [neg_config, pos_config]
+
 def generate_children_current_config_at_position(configs, position):
     out_configs = []
     for config in configs:
         out_configs.extend(generate_children_at_position(config, position))
     return out_configs
 
-print(generate_children_current_config_at_position([spin1,spin2], 1))
+config = [[]]
+for index in range(3):
+    config_list = [add_element(current_config) for current_config in config]
+    print(config_list)
+    config = [val for sublist in config_list for val in sublist]
+    print(config)
