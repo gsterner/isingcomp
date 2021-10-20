1dd6b75464b046ffd04d2e58e55eb21047ac2505
Gustaf Lundborg <gsterner@gmail.com>
2021-10-20 22:28:11 +0200

    Adds sweep function for longer simulations

--- src/simulation_tools.py
+++ src/simulation_tools.py
@@ -2,21 +2,22 @@ import hamiltonian as ha
 import generate_system as gs
 import random
 import copy
+import math
 
-def satisfies_boltzman_draw():
-    return random.random() < 0.01
+def satisfies_boltzman_draw(temperature):
+    return random.random() < temperature
 
 def is_flip_lower_energy(index, spins, spins_flip, connections):
     energy = ha.spin_hamiltonian_of_index(index, spins, connections)
     energy_flip = ha.spin_hamiltonian_of_index(index, spins_flip, connections)
     return energy_flip < energy
 
-def is_flip(index, spins, spins_flip, connections):
+def is_flip(index, spins, spins_flip, connections, temperature):
     energy = ha.spin_hamiltonian_of_index(index, spins, connections)
     energy_flip = ha.spin_hamiltonian_of_index(index, spins_flip, connections)
     if is_flip_lower_energy(index, spins, spins_flip, connections):
         return True
-    elif satisfies_boltzman_draw():
+    elif satisfies_boltzman_draw(temperature):
         return True
     else:
         return False
@@ -29,24 +30,34 @@ def flip_spin(index, spins):
 def randomize_index(spins):
     return random.randint(0, len(spins) - 1)
 
-def sim_step(spins, connections):
+def sim_step(spins, connections, temperature):
     #pick random site
     index = randomize_index(spins)
     #flip that spin
     spins_flip = flip_spin(index, spins)
     #check if that should be kept and update
-    is_flip_val = is_flip(index, spins, spins_flip, connections)
+    is_flip_val = is_flip(index, spins, spins_flip, connections, temperature)
     #print("flip", is_flip_val, spins_flip)
     if is_flip_val:
         return spins_flip
     else:
         return spins
 
+def sweep(spins, connections, temperature):
+    for i in range(len(spins)):
+        spins = sim_step(spins, connections, temperature)
+    return spins
+
 def spin_to_char(spin):
     if spin == -1:
         return '-'
     else:
         return '+'
+
+def setup_nearest_neigbour_system(system_size, rows):
+    spins = gs.create_randomized_spins(system_size)
+    connections = gs.create_constant_nearest_neighbour_connections(rows, rows, 1)
+    return spins, connections
 
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
@@ -56,13 +67,14 @@ def pp_spin_system(spins, row_size):
     for row in chunker(spins_as_chars, row_size):
         print(''.join(row))
 
-N = 100
-rows = 10
-S = gs.create_randomized_spins(N)
-J = gs.create_constant_nearest_neighbour_connections(rows, rows, 1)
+#######################
+N = 900
+T = 0.1
+rows = int(math.sqrt(N))
+S,J = setup_nearest_neigbour_system(N, rows)
 pp_spin_system(S, rows)
-print('____________________')
-for i in range(1000):
-    S = sim_step(S,J)
+print('_'*rows)
+for i in range(100):
+    S = sweep(S, J, T)
     pp_spin_system(S, rows)
-    print('_____________________________')
+    print('_'*rows, sum(S))
--- tests/simulation_unit_test.py
+++ tests/simulation_unit_test.py
@@ -4,7 +4,7 @@ import simulation_tools
 import generate_system
 
 def test_satisfies_boltzman_draw():
-    draw = simulation_tools.satisfies_boltzman_draw()
+    draw = simulation_tools.satisfies_boltzman_draw(0.5)
     assert draw == True or draw == False
 
 def test_flip_spin():
@@ -23,7 +23,7 @@ def test_is_flip():
     S = [1,1,1,1]
     S_flip = simulation_tools.flip_spin(2,S)
     J = generate_system.create_constant_nearest_neighbour_connections(2,2,1)
-    is_flip_val = simulation_tools.is_flip(2, S, S_flip, J)
+    is_flip_val = simulation_tools.is_flip(2, S, S_flip, J, 0.5)
     assert is_flip_val == True or is_flip_val == False
 
 def test_randomize_index():
@@ -35,5 +35,5 @@ def test_randomize_index():
 def test_sim_step():
     S = [-1,1,1,-1]
     J = generate_system.create_constant_nearest_neighbour_connections(2,2,1)
-    S_step = simulation_tools.sim_step(S, J)
+    S_step = simulation_tools.sim_step(S, J, 0.5)
     assert S_step != S
