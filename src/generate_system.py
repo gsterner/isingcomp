a2679532f05f7629ca4dedf9572eb387532db16a
Gustaf Lundborg <gsterner@gmail.com>
2021-10-31 21:31:50 +0100

    Adds sketch for adding periodic connections

--- src/generate_system.py
+++ src/generate_system.py
@@ -47,6 +47,46 @@ def create_constant_nearest_neighbour_connections(rows, cols, connection_value):
         connection_matrix.append(row_list)
     return connection_matrix
 
+def has_periodic_right_connection(j_row, j_col, spin_dimension):
+    if j_col == spin_dimension:
+        return True
+    return False
+
+def has_periodic_left_connection(j_row, j_col, spin_dimension):
+    if j_row % spin_dimension == 0:
+        return True
+    return False
+
+def has_periodic_down_connection(j_row, j_col, spin_dimension):
+    if j_row == spin_dimension:
+        return True
+    return False
+
+def has_periodic_up_connection(j_row, j_col, spin_dimension):
+    if j_row == 0:
+        return True
+    return False
+
+def has_periodic_connection(j_row, j_col, spin_dimension):
+    return has_periodic_right__connection(j_row, j_col, spin_dimension) or has_periodic_left_connection(j_row, j_col, spin_dimension) or has_periodic_down_connection(j_row, j_col, spin_dimension) or has_periodic_up_connection(j_row, j_col, spin_dimension)
+
+def create_periodic_nearest_neighbour_connections(rows, cols, connection_value):
+    #assume square spin matrix rows = cols
+    spin_dimension = rows
+    dimension = rows * cols
+    connection_matrix = []
+    for j_row in range(dimension):
+        row_list = []
+        for j_col in range(dimension):
+            if has_periodic_connection(j_row, j_col, spin_dimension):
+                row_list.append(connection_value)
+            else:
+                row_list.append(0)
+        connection_matrix.append(row_list)
+    return connection_matrix
+
+
+
 def pp_connection_matrix(connection_matrix):
     for j_row_list in connection_matrix:
         print(j_row_list)
