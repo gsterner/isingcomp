36b98b77e6fd4171ab126827b7d5253f19bf50e2
Gustaf Lundborg <gsterner@gmail.com>
2022-07-17 16:11:37 +0200

    Move simulation call on step up in call stack

--- src/runner_utils.py
+++ src/runner_utils.py
@@ -5,13 +5,10 @@ import math
 def percent(part, full):
     return str(part/full * 100) + "%"
 
-def simulation(N, Q, T, SIMS):
-    #SIMS = 1
-    S,J = sim.setup_nearest_neigbour_system(N, int(math.sqrt(N)))
+def simulation(S, J, Q, T, SIMS):
     magnetization = []
     for s in range(SIMS):
         m = equilib.equilibriate(S, J, Q, T)
-        #print(percent(s, SIMS))
         magnetization.append(m)
     return magnetization
 
@@ -22,7 +19,8 @@ def calc_abs_magnetization(magnet, cut_off_fraction):
     return sum(cut_off_magnet)/len(cut_off_magnet)
 
 def mean_over_simulations(N, Q, T, sims, cut_off_fraction):
-    M = simulation(N, Q, T, sims)
+    S,J = sim.-setup_nearest_neigbour_system(N, int(math.sqrt(N)))
+    M = simulation(S, J, Q, T, sims)
     abs_m = []
     for m in M:
         abs_m.append(calc_abs_magnetization(m, cut_off_fraction))
