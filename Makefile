de21a77eeb871f1b9c5c1586ed53c97202050f06
Gustaf Lundborg <gsterner@gmail.com>
2022-07-19 23:21:08 +0200

    Make equilibrium command line call

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 isingsim:
 	python3 src/isingsim.py data/test_ising_sim.json
+
+system-setup-test:
+	python3 src/system_setup.py 16 system_16.json
+
+equilibrium-test:
+	python3 src/equilibrium.py data/system_16.json 100 0.16
--- src/equilibrium.py
+++ src/equilibrium.py
@@ -1,12 +1,36 @@
 import simulation_tools as sim
+import argparse
+import json
 
+def equilibrium(spins_in, connections, sweeps, temperature):
+    spins = spins_in
+    for i in range(sweeps):
+        spins = sim.sweep(spins, connections, temperature)
+    return spins
+
+#TODO clean this function up. change name and variables. should magnetization be calculated like this???
 def equilibriate(S, J, Q, T):
-    #S,J = sim.setup_nearest_neigbour_system(N, int(math.sqrt(N)))
-    #rows = int(math.sqrt(len(S)))
     magnetization =  []
-    #sim.pp_spin_system(S, rows)
     for i in range(Q):
         S = sim.sweep(S, J, T)
         magnetization.append(sim.magnetization(S))
-    #sim.pp_spin_system(S, rows)
     return magnetization
+
+def main():
+    parser = argparse.ArgumentParser(description='Run ising equilibrium from input file')
+    parser.add_argument('system_file', metavar='system_file', type=str, help='System File')
+    parser.add_argument('sweeps_to_equilibrium', metavar='sweeps_to_equilibrium', type=int, help='Number of sweeps to reach equilibrium')
+    parser.add_argument('temperature', metavar='temperature', type=float, help='Temperature')
+    args = parser.parse_args()
+    f_sys = open(args.system_file)
+    system = json.load(f_sys)
+    f_sys.close()
+
+    spins = equilibrium(system["spins"],
+                        system["connections"],
+                        args.sweeps_to_equilibrium,
+                        args.temperature)
+    print(args.temperature, sim.magnetization(spins))
+
+if __name__ == "__main__":
+    main()
