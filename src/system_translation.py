06a57b0365b05d7e67af014cf9b2990a9b2c4c1d
Gustaf Lundborg <gsterner@gmail.com>
2022-09-15 14:27:35 +0200

    Add skeleton for translation several random walks to spins

--- src/system_translation.py
+++ src/system_translation.py
@@ -1,5 +1,6 @@
 import polymersim as polsim
 from polymersim import Direction
+import simulation_tools
 import data_utils
 import argparse
 import json
@@ -68,13 +69,32 @@ def translate_positions_to_spins(positions):
         spins.extend(spin_pair)
     return spins
 
+def order_postions(positions_raw):
+    number_of_configs = int(len(positions_raw[0])/2)
+    grouped_rows = []
+    for pos in positions_raw:
+        grouped_rows.append(list(simulation_tools.chunker(pos,2)))
+    positions_ordered = []
+    for config in range(number_of_configs):
+        config_positions = []
+        for row in grouped_rows:
+            config_positions.append(row[config])
+        positions_ordered.append(config_positions)
+    return positions_ordered
+
+
+def translate_position_configs_to_spins(data):
+    positions_raw = convert_list_list_char_to_int(data)
+    positions = order_postions(positions_raw)
+    print(positions)
+
 def main():
     parser = argparse.ArgumentParser(description='Translate spin system to random walk')
     parser.add_argument('--translation',
                         default='spins_to_positions',
                         const='spins_to_positions',
                         nargs='?',
-                        choices=['positions_to_spins', 'spins_to_positions'],
+                        choices=['positions_to_spins', 'spins_to_positions', 'position_configs_to_spins'],
                         help='Choose translation direction (default: %(default)s)')
     parser.add_argument('input_file', metavar='input_file', type=str, help='Input File')
     parser.add_argument('output_file', metavar='output_file', type=str, help='Output File')
@@ -87,7 +107,7 @@ def main():
         spin_system = input_data["spins"]
         polymer_positions = translate_spins_to_positions(spin_system)
         polsim.dump_random_walk_to_csv(args.output_file, polymer_positions)
-    else:
+    elif args.translation == 'positions_to_spins':
         f = open(args.input_file)
         reader = csv.reader(f)
         input_data= list(reader)
@@ -95,6 +115,13 @@ def main():
         positions_in = convert_list_list_char_to_int(input_data)
         spin_system = translate_positions_to_spins(positions_in)
         data_utils.save_spins(spin_system, args.output_file)
+    else:
+        f = open(args.input_file)
+        reader = csv.reader(f)
+        input_data= list(reader)
+        f.close()
+        translate_position_configs_to_spins(input_data)
+
 
 if __name__ == "__main__":
     main()
