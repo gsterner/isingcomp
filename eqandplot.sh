f99be1f6375f4b8c5359d8e9050b600201da5b02
Gustaf Lundborg <gsterner@gmail.com>
2022-09-25 21:26:09 +0200

    Interesting numbers to test

--- eqandplot.sh
+++ eqandplot.sh
@@ -1,6 +1,6 @@
 python3 src/system_translation.py translated.json data/test10/start.csv
 gnuplot -e "filename='start.csv'; p_range=12; set title 'START'; show title" -p src/plotrandomwalk.gnuplot
-python3 src/equilibrium.py translated.json data/test10/connections_all.json 500 0.5
-#python3 src/equilibrium.py translated.json data/test10/equal_connections.json 500 0.5
+python3 src/equilibrium.py translated.json data/test10/connections_all.json 1000 0.01
+#python3 src/equilibrium.py translated.json data/test10/equal_connections.json 1000 0.01
 python3 src/system_translation.py spins_output.json end.csv
 gnuplot -e "filename='end.csv'; p_range=12; set title 'END'; show title" -p src/plotrandomwalk.gnuplot
