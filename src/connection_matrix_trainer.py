974ee28c545f9f4159d39567e100578c337838a3
Gustaf Lundborg <gsterner@gmail.com>
2022-10-05 13:41:15 +0200

    Finalize matrix trainer

--- src/connection_matrix_trainer.py
+++ src/connection_matrix_trainer.py
@@ -1,61 +1,60 @@
+import data_utils
 import polymersim as poly
 import system_translation as systrans
 import connection_matrix_creation as cmc
 import polymerstatistics as polstat
 import numpy as np
 from numpy import linalg
+import argparse
 
-walk_length = 5
-spin_dim = 2 * walk_length
-no_test_walks = 10000
-connections_total = np.zeros((spin_dim, spin_dim))
-unallowed_count = 0
+def is_unallowed(polymer_positions):
+    return polstat.has_duplicates(polymer_positions)
 
-#why does it work with 4 but not the others???
+def is_covered(polymer_positions, unallowed_walks):
+    return polymer_positions.__str__() in unallowed_walks
 
-#Find a way to skip those that have already been done???
-unallowed_walks = set()
-for i in range(no_test_walks):
-    polymer_positions = poly.random_walk(walk_length)
-    is_unallowed = polstat.has_duplicates(polymer_positions)
-    if is_unallowed and polymer_positions.__str__() not in unallowed_walks:
-        unallowed_walks.add(polymer_positions.__str__())
-        spin_system = systrans.translate_positions_to_spins(polymer_positions)
-        connections = cmc.outer_square(spin_system)
-        connections_total += connections
-        unallowed_count += 1
+def single_connection_matrix_from_random_walk(polymer_positions):
+    spin_system = systrans.translate_positions_to_spins(polymer_positions)
+    return cmc.outer_square(spin_system)
 
-for walk in unallowed_walks:
-    print(walk)
+def train_connection_matrix(spin_dim, walk_length, no_test_walks):
+    percent_step = no_test_walks / 100
+    percent_count = 1
+    connections_total = np.zeros((spin_dim, spin_dim))
+    unallowed_walks = set()
+    for i in range(no_test_walks):
+        polymer_positions = poly.random_walk(walk_length)
+        if is_unallowed(polymer_positions) and not is_covered(polymer_positions, unallowed_walks):
+            unallowed_walks.add(polymer_positions.__str__())
+            connections_total += single_connection_matrix_from_random_walk(polymer_positions)
+        if i > (percent_step * percent_count):
+            print(percent_count, "%")
+            percent_count += 1
+    return unallowed_walks, connections_total
 
-print(connections_total)
-connections_total = connections_total / unallowed_count
-print("unalllowed",unallowed_count)
-print("norm", linalg.norm(connections_total))
-print(connections_total)
-true_4 = np.array([
-    [-4, 0, 4, 0],
-    [0, -4, 0, 4],
-    [4, 0, -4, 0],
-    [0, 4, 0, -4]
-])
-true_4 = true_4 / 4
-print("true 4 norm", linalg.norm(true_4))
-print(true_4)
+def connection_matrix_simulation(walk_length, no_test_walks):
+    spin_dim = 2 * walk_length
+    return train_connection_matrix(spin_dim, walk_length, no_test_walks)
 
-true_6 = np.array([
-    [-6, 0, 6, 0, 0, 0],
-    [0, -6, 0, 6, 0, 0],
-    [6, 0, -6, 0, 6, 0],
-    [0, 6, 0, -6, 0, 6],
-    [0, 0, 6, 0, -6, 0],
-    [0, 0, 0, 6, 0, -6]
-])
-true_6 = true_6 / 6
-print("true 6 norm", linalg.norm(true_6))
+def normalize_connection_matrix(connections, no_unallowed_walks):
+    return connections / no_unallowed_walks
 
-true_10 = np.array([[-1004476, 0, 15484, 0, 8484, 0, 4284, 0, 3932, 0, 2492, 0, 1980, 0, 1108, 0, 732, 0, 196, 0], [0, -1004476, 0, 15484, 0, 8484, 0, 4284, 0, 3932, 0, 2492, 0, 1980, 0, 1108, 0, 732, 0, 196], [15484, 0, -1004476, 0, 16396, 0, 12340, 0, 7236, 0, 5668, 0, 4020, 0, 2748, 0, 1684, 0, 732, 0], [0, 15484, 0, -1004476, 0, 16396, 0, 12340, 0, 7236, 0, 5668, 0, 4020, 0, 2748, 0, 1684, 0, 732], [8484, 0, 16396, 0, -1004476, 0, 17612, 0, 12684, 0, 8588, 0, 6508, 0, 4356, 0, 2748, 0, 1108, 0], [0, 8484, 0, 16396, 0, -1004476, 0, 17612, 0, 12684, 0, 8588, 0, 6508, 0, 4356, 0, 2748, 0, 1108], [4284, 0, 12340, 0, 17612, 0, -1004476, 0, 17396, 0, 13236, 0, 8580, 0, 6508, 0, 4020, 0, 1980, 0], [0, 4284, 0, 12340, 0, 17612, 0, -1004476, 0, 17396, 0, 13236, 0, 8580, 0, 6508, 0, 4020, 0, 1980], [3932, 0, 7236, 0, 12684, 0, 17396, 0, -1004476, 0, 17476, 0, 13236, 0, 8588, 0, 5668, 0, 2492, 0], [0, 3932, 0, 7236, 0, 12684, 0, 17396, 0, -1004476, 0, 17476, 0, 13236, 0, 8588, 0, 5668, 0, 2492], [2492, 0, 5668, 0, 8588, 0, 13236, 0, 17476, 0, -1004476, 0, 17396, 0, 12684, 0, 7236, 0, 3932, 0], [0, 2492, 0, 5668, 0, 8588, 0, 13236, 0, 17476, 0, -1004476, 0, 17396, 0, 12684, 0, 7236, 0, 3932], [1980, 0, 4020, 0, 6508, 0, 8580, 0, 13236, 0, 17396, 0, -1004476, 0, 17612, 0, 12340, 0, 4284, 0], [0, 1980, 0, 4020, 0, 6508, 0, 8580, 0, 13236, 0, 17396, 0, -1004476, 0, 17612, 0, 12340, 0, 4284], [1108, 0, 2748, 0, 4356, 0, 6508, 0, 8588, 0, 12684, 0, 17612, 0, -1004476, 0, 16396, 0, 8484, 0], [0, 1108, 0, 2748, 0, 4356, 0, 6508, 0, 8588, 0, 12684, 0, 17612, 0, -1004476, 0, 16396, 0, 8484], [732, 0, 1684, 0, 2748, 0, 4020, 0, 5668, 0, 7236, 0, 12340, 0, 16396, 0, -1004476, 0, 15484, 0], [0, 732, 0, 1684, 0, 2748, 0, 4020, 0, 5668, 0, 7236, 0, 12340, 0, 16396, 0, -1004476, 0, 15484], [196, 0, 732, 0, 1108, 0, 1980, 0, 2492, 0, 3932, 0, 4284, 0, 8484, 0, 15484, 0, -1004476, 0], [0, 196, 0, 732, 0, 1108, 0, 1980, 0, 2492, 0, 3932, 0, 4284, 0, 8484, 0, 15484, 0, -1004476]])
+def main():
+    parser = argparse.ArgumentParser(description='Simulate connection matrix')
+    parser.add_argument('walk_length',
+                        metavar='walk_length',
+                        type=int,
+                        help='Number of Steps')
+    parser.add_argument('number_test_walks',
+                        metavar='number_test_walks',
+                        type=int,
+                        help='Number walks used in simulation')
+    parser.add_argument('output_file', metavar='output_file', type=str, help='Output File')
+    args = parser.parse_args()
+    unallowed_walks, connections_total = connection_matrix_simulation(args.walk_length, args.number_test_walks)
+    data_utils.save_connections(connections_total.tolist(), args.output_file)
+    print("Number of unalllowed walks",len(unallowed_walks))
+    print("Matrix Norm", linalg.norm(connections_total))
 
-true_10 = true_10 / 1004476
-print("true unallowed", 1004476)
-print("true norm", linalg.norm(true_10))
+if __name__ == "__main__":
+    main()
