8bf54045dd8c67c2cff01f317fb6cd2e078c231b
Gustaf Lundborg <gsterner@gmail.com>
2022-11-07 13:50:09 +0100

    Simplify Tree Node

--- src/walktree.py
+++ src/walktree.py
@@ -3,40 +3,19 @@ from polymersim import Direction
 
 class Node():
     def __init__(self):
-        self.north = None
-        self.south = None
-        self.east = None
-        self.west = None
+        self.children = {Direction.NORTH: None,
+                         Direction.SOUTH: None,
+                         Direction.EAST: None,
+                         Direction.WEST: None}
 
     def add_child(self, direction):
-        if direction == Direction.NORTH:
-            self.north = Node()
-        if direction == Direction.SOUTH:
-            self.south = Node()
-        if direction == Direction.EAST:
-            self.east = Node()
-        if direction == Direction.WEST:
-            self.west = Node()
+        self.children[direction] = Node()
 
     def get_child(self, direction):
-        if direction == Direction.NORTH:
-            return self.north
-        if direction == Direction.SOUTH:
-            return self.south
-        if direction == Direction.EAST:
-            return self.east
-        if direction == Direction.WEST:
-            return self.west
+        return self.children[direction]
 
     def has_child(self, direction):
-        if direction == Direction.NORTH:
-            return self.north != None
-        if direction == Direction.SOUTH:
-            return self.south != None
-        if direction == Direction.EAST:
-            return self.east != None
-        if direction == Direction.WEST:
-            return self.west != None
+        return self.children[direction] != None
 
 # test_walk = [Direction.NORTH, Direction.WEST]
 
