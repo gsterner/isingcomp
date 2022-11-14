7f67483f6314551ef48a92ed06b6abd356450e2f
Gustaf Lundborg <gsterner@gmail.com>
2022-11-14 22:26:02 +0100

    Add temperature profile to annealing

--- src/anneal_polymer.py
+++ src/anneal_polymer.py
@@ -22,6 +22,18 @@ def anneal_polymer_positions(polymer_positions, connections, sweeps, temperature
                                           temperature)
     return systrans.translate_spins_to_positions(numba.typed.List(equilibrium_spins))
 
+def anneal_polymer_positions_profile(polymer_positions, connections, sweeps_profile, temperature_profile):
+    numba_positions = numba.typed.List()
+    for pos in polymer_positions:
+        numba_positions.append(numba.typed.List(pos))
+    spins = systrans.translate_positions_to_spins(numba_positions)
+    for p_index in range(len(sweeps_profile)):
+        spins = equil.equilibrium(spins,
+                                  connections,
+                                  sweeps_profile[p_index],
+                                  temperature_profile[p_index])
+    return systrans.translate_spins_to_positions(numba.typed.List(spins))
+
 def convert_histogram_edges_to_mids(edges):
     distance = edges[1:] - edges[:-1]
     mids = edges[:-1] + distance / 2
@@ -32,9 +44,9 @@ def main():
     parser.add_argument('polymer_positions_in', metavar='polymer_positions_in', type=str, help='Random walk file in')
     parser.add_argument('connection_file', metavar='connection_file', type=str, help='Connection File')
     parser.add_argument('output_file', metavar='output_file', type=str, help='Output File ')
-    parser.add_argument('sweeps_to_equilibrium', metavar='sweeps_to_equilibrium', type=int, help='Number of sweeps to reach equilibrium')
+    parser.add_argument('temperature_profile', metavar='temperature_profile', type=str, help='Temperature and sweeps profile file in')
     parser.add_argument('number_runs', metavar='number_runs', type=int, help='Number of runs')
-    parser.add_argument('temperature', metavar='temperature', type=float, help='Temperature')
+
     args = parser.parse_args()
     f_polyms = open(args.polymer_positions_in)
     reader = csv.reader(f_polyms)
@@ -43,6 +55,9 @@ def main():
     f_connections = open(args.connection_file)
     connections = json.load(f_connections)
     f_connections.close()
+    f_temp_profile = open(args.temperature_profile)
+    temperature_profile = json.load(f_temp_profile)
+    f_temp_profile.close()
 
     polymer_positions_start = data_utils.convert_list_list_char_to_int(polymer_positions_strings)
     start_clashes = polstat.count_clashes(polymer_positions_start)
@@ -50,10 +65,10 @@ def main():
     percent_step = args.number_runs / 100
     percent_count = 1
     for i in range(args.number_runs):
-        polymer_positions_end =  anneal_polymer_positions(polymer_positions_start,
-                                                          connections["connections"],
-                                                          args.sweeps_to_equilibrium,
-                                                          args.temperature)
+        polymer_positions_end =  anneal_polymer_positions_profile(polymer_positions_start,
+                                                                  connections["connections"],
+                                                                  temperature_profile["sweeps"],
+                                                                  temperature_profile["temperature"])
         end_clashes.append(polstat.count_clashes(polymer_positions_end))
         if i > (percent_step * percent_count):
             print(percent_count, "%")
